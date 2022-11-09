25b1b6a3991ad13903e3da1685ec668e29e7660b
millsjdm56 <101364340+millsjdm56@users.noreply.github.com>
2022-11-09 14:32:54 -0500

    Update func.py

--- func.py
+++ func.py
@@ -13,7 +13,7 @@ def handler(ctx, data: io.BytesIO=None):
     except (Exception, ValueError) as ex:
         logging.getLogger().info('error parsing json payload: ' + str(ex))
 
-    logging.getLogger().info("Inside the Python Hello World function")
+    logging.getLogger().info("Inside Python Hello World function")
     return response.Response(
         ctx, response_data=json.dumps(
             {"message": "Hello {0}".format(name)}),
